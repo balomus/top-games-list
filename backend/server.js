d53e7d519796ce0c2a17350fa26cc129898c915f
Stephen Mercier <balom.us@gmail.com>
2022-07-26 15:49:21 -0400

    Basic backend setup. Added scripts to package.json file.

--- backend/server.js
+++ backend/server.js
@@ -2,3 +2,14 @@ const express = require("express");
 const dotenv = require("dotenv").config();
 const port = process.env.PORT || 5000;
 
+const app = express();
+
+// These two middleware enable us to parse json and urlencoded responses on API calls
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.get('/api', (req, res) => {
+    res.json({ message: 'response!' });
+})
+
+app.listen(port, () => console.log(`Server started on port ${port}`));
