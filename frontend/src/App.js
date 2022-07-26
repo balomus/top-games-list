10979e54df0782b29e8c15af75d97994b5544bf9
Stephen Mercier <balom.us@gmail.com>
2022-07-26 14:46:13 -0400

    Initial commit

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import GameLookup from './components/GameLookup';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>App</h1>
+      <GameLookup />
+    </div>
+  );
+}
+
+export default App;
---
+++ frontend/src/components/GameLookup.jsx
@@ -0,0 +1,45 @@
+import { useEffect, useState } from "react";
+
+function GameLookup() {
+  const [formData, setFormData] = useState("");
+
+  const lookupResults = ["test1", "test2"];
+
+  const onChange = (e) => {
+    setFormData((e.target.value))
+  }
+
+  useEffect(() => {
+    if (formData !== "")
+    {
+      
+    }
+  }, [formData]);
+
+  useEffect(() => {
+    authenticateAPI();
+  }, [])
+
+  const authenticateAPI = async () => {
+    let response = await fetch (`https://id.twitch.tv/oauth2/token?client_id=${process.env.TWITCH_CLIENT_ID}&client_secret=${process.env.TWITCH_CLIENT_SECRET}&grant_type=${process.env.GRANT_TYPE}`);
+    let data = await response.json();
+    console.log(data);
+  }
+  
+  return (
+    <div>
+        <h2>Game Lookup</h2>
+        <form>
+            <label>
+                Enter a game:
+                <input type="text" name="game" value={formData} onChange={onChange} />
+            </label>
+        </form>
+        {lookupResults.map((result) => (
+          <div key={result}>{result}</div>
+        ))}
+    </div>
+  )
+}
+
+export default GameLookup
