939fb3a84ae56e97df38f882607e06a5c7271407
balomus <balom.us@gmail.com>
2023-02-06 19:16:20 -0500

    Implemented logic for owner of game list

--- frontend/src/pages/GameList.jsx
+++ frontend/src/pages/GameList.jsx
@@ -7,6 +7,7 @@ import "./GameList.css";
 import { getGameLists, updateGameList } from "../features/gameLists/gameListSlice";
 import { useSelector, useDispatch } from 'react-redux';
 import GameLookup from "../components/GameLookup";
+import axios from 'axios';
 
 const GameList = () => {
 
@@ -14,14 +15,63 @@ const GameList = () => {
     const [searchParams] = useSearchParams();
     const { gameLists, isLoading } = useSelector((state) => state.gameLists);
     const [localGameList, setLocalGameList] = useState();
+
+    const [userID, setUserID] = useState()
+    const [owner, setOwner] = useState(false);
+
+    const checkUser = () => {
+        if (typeof window !== "undefined" && localGameList !== undefined)
+        {
+            const user = JSON.parse(localStorage.getItem("user"));
+            if (user === null)
+            {
+                setUserID("unauthenticated");
+                setOwner(false);
+            }
+            else
+            {
+                setUserID(user._id);
+                
+                if (user._id.toString() === localGameList.user.toString())
+                {
+                    setOwner(true);
+                }
+            }
+        }
+
+    }
+
+    // useEffect(() => {
+    //     checkUser();
+    // }, [])
     
     useEffect(() => {
         dispatch(getGameLists());
     }, [dispatch])
 
+    // useEffect(() => {
+    //     if (user === null)
+    //     {
+    //         setOwner(false);
+    //     }
+    //     else
+    //     {
+    //         if (user._id === localGameList.user)
+    //         {
+    //             setOwner(true);
+    //         }
+    //     }
+    // }, [user])  
+
     useEffect(() => {
-        const found = gameLists.find(e => e._id === searchParams.get('id'));
-        setLocalGameList(found);
+        // const found = gameLists.find(e => e._id === searchParams.get('id'));
+        const fetchData = async () => {
+            const response = await axios.get('/api/gamelists/' + searchParams.get('id'));
+            setLocalGameList(response.data);
+            checkUser();
+            console.log("test");
+        }
+        fetchData();
     }, [gameLists, searchParams])
 
     const onChange = (e) => {
@@ -94,41 +144,68 @@ const GameList = () => {
             <section className="gamelist">
                 {localGameList ? (
                     <>
-                        ID: {localGameList._id}
+                        userID: {userID}<br></br>
+                        GameList Owner? {owner.toString()}<br></br>
+                        Gamelist ID: {localGameList._id}
                         <h2>
-                            <input type="text" name="title" value={localGameList.title} onChange={onChange} />
+                            { owner ?
+                                <input type="text" name="title" value={localGameList.title} onChange={onChange} />
+                            : 
+                                <>{localGameList.title}</>
+                            }
+                            
                             {/* {localGameList.title} */}
                         </h2>
-                        <div><input type="text" name="description" value={localGameList.description} onChange={onChange} /></div>
+                        <h3>
+                            { owner ? 
+                                <input type="text" name="description" value={localGameList.description} onChange={onChange} />
+                            : 
+                                <>{localGameList.description}</>
+                            }
+                            
+                        </h3>
                         {localGameList.games.map((game, index) => {
                             return(
                                 <div className="game" key={game.id}>
                                     <h3>
                                         {index + 1}. {game.name}
                                     </h3>
+
+                                    { owner && 
                                     <div>
                                         <div><button onClick={() => handleClick('up', index)}><FaArrowUp /></button></div>
                                         <div><button onClick={() => handleClick('delete', index)} className="x"><HiX /></button></div>
                                         <div><button onClick={() => handleClick('down', index)}><FaArrowDown /></button></div>
                                     </div>
+                                    }
+
                                     <div><img src={game.url} alt={`${game.name} cover art`} className="game-image"></img></div>
                                 </div>
                             );
                         })}
-                        {JSON.stringify(localGameList) === JSON.stringify(gameLists.find(e => e._id === searchParams.get('id'))) ? (
-                            <button disabled>Save</button>
-                        ) : (
-                            <button onClick={handleSave}>Save</button>
-                        )}
+
+                        { owner &&
+                            <div>
+                                {JSON.stringify(localGameList) === JSON.stringify(gameLists.find(e => e._id === searchParams.get('id'))) ? (
+                                    <button disabled>Save</button>
+                                ) : (
+                                    <button onClick={handleSave}>Save</button>
+                                )}
+                            </div>
+                        }
+                        
                         
                     </>
                 ) : (
                     <Spinner />
                 )}
             </section>
+            { owner &&
             <section className="gameLookup">
                 <GameLookup localGameList={localGameList} setLocalGameList={setLocalGameList} />
             </section>
+            }
+            
         </>
      );
 }
